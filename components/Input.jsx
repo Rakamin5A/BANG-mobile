0515545c16d47bcaab1536d1f6c193d55ffd8264
Damar Buana Murti <damarbuanamurti@gmail.com>
2024-12-20 21:55:03 +0700

    feat: add input type

--- components/Input.jsx
+++ components/Input.jsx
@@ -3,6 +3,8 @@ import { StyleSheet, TextInput } from "react-native";
 export default function Input({
   name,
   placeholder,
+  type = "default",
+  isPassword = false,
   state,
   handleOnChangeText,
 }) {
@@ -11,18 +13,23 @@ export default function Input({
       value={state}
       placeholder={placeholder}
       placeholderTextColor="#FFCFAC"
+      keyboardType={type}
+      autoCapitalize="none"
       style={styles.input}
       onChangeText={(value) => handleOnChangeText(name, value)}
+      secureTextEntry={isPassword}
     />
   );
 }
 
 const styles = StyleSheet.create({
   input: {
+    color: "white",
     borderWidth: 2,
     borderColor: "#FFCFAC",
     borderRadius: 15,
     paddingLeft: 10,
     width: 315,
+    padding: 16,
   },
 });
